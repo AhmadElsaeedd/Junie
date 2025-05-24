f92512f3f741a39774e03e78025b7f537cc6dbee
AhmadElsaeedd <ae2200@nyu.edu>
2025-05-24 18:11:54 +0400

    Initial Commit

---
+++ Junie/JunieApp.swift
@@ -0,0 +1,17 @@
+//
+//  JunieApp.swift
+//  Junie
+//
+//  Created by Ahmed Elsaeed on 24/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct JunieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JunieTests/JunieTests.swift
@@ -0,0 +1,17 @@
+//
+//  JunieTests.swift
+//  JunieTests
+//
+//  Created by Ahmed Elsaeed on 24/05/2025.
+//
+
+import Testing
+@testable import Junie
+
+struct JunieTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
