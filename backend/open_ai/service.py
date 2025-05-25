51b0aed00f7d2a476f62d34c0f6248e59c1f6158
AhmadElsaeedd <ae2200@nyu.edu>
2025-05-25 09:46:07 +0400

    feat: created `OpenAIService` to use for transcription and settings package to load environment variables

---
+++ backend/open_ai/service.py
@@ -0,0 +1,25 @@
+import logging
+from typing import Final
+
+from openai import OpenAI
+from openai.types.audio.transcription import Transcription
+from settings.variables import OPENAI_API_KEY
+
+logger: Final[logging.Logger] = logging.getLogger(__name__)
+
+class OpenAIService:
+    def __init__(self):
+        self.client: Final[OpenAI] = OpenAI(api_key=OPENAI_API_KEY)
+
+    def transcribe(self, *, file_content: bytes, filename: str) -> str:
+        """
+        Calls the OpenAI API to transcribe an audio file and returns the transcription.
+        """
+        logger.info(f"Processing file '{filename}' through OpenAI API.")
+        
+        transcription: Final[Transcription] = self.client.audio.transcriptions.create(
+            file=(filename, file_content),
+            model="whisper-1",
+        )
+        
+        return transcription.text
