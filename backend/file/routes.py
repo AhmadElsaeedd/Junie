528a2f0fef99a3d9c0eec54f9228765d49b6e981
AhmadElsaeedd <ae2200@nyu.edu>
2025-05-25 10:24:35 +0400

    refactor: changed upload audio file endpoint to make it a transcription endpoint

--- backend/file/routes.py
+++ backend/file/routes.py
@@ -4,35 +4,42 @@ from typing import Final
 from fastapi import APIRouter, HTTPException, UploadFile
 from fastapi.responses import JSONResponse
 from file.service import FileService
+from file.utils import FileUtils
 
 logger: Final[logging.Logger] = logging.getLogger(__name__)
 
 # Define the router
 files_router: Final[APIRouter] = APIRouter()
 
-@files_router.post("/upload_audio/", tags=["File Operations"])
-async def upload_audio_file(audio_file: UploadFile):
+__SUCCESS_MESSAGE: Final[str] = "Successfully transcribed file."
+__MESSAGE_KEY: Final[str] = "message"
+__TRANSCRIPTION_KEY: Final[str] = "transcription"
+
+@files_router.post("/transcribe_audio/", tags=["File Operations"])
+async def transcribe_audio_file(audio_file: UploadFile):
     """
-    TODO: Add docstring
+    Transcribes the audio file and returns the transcription.
     """
-    target_file_name: Final[str | None] = audio_file.filename
+    FileUtils.assert_valid_file(file=audio_file)
     
-    if not audio_file or target_file_name is None:
-        raise HTTPException(status_code=400, detail="No file or filename was uploaded.")
+    target_file_name: Final[str] = audio_file.filename
 
     try:        
         logger.info(f"File '{target_file_name}' received.")
-        FileService.transcribe_audio_file(file=audio_file)
+        transcription: Final[str] = FileService.transcribe_audio_file(file=audio_file)
         
         return JSONResponse(
-            content={"message": f"File '{target_file_name}' successfully uploaded."}, 
+            content={
+                __MESSAGE_KEY: __SUCCESS_MESSAGE,
+                __TRANSCRIPTION_KEY: transcription,
+            }, 
             status_code=200,
         )
     except Exception as e:
-        logger.error(f"Error saving file '{target_file_name}': {e}")
+        logger.error(f"Error transcribing file '{target_file_name}': {e}")
         raise HTTPException(
             status_code=500,
-            detail=f"Could not save file '{target_file_name}'. Error: {str(e)}",
+            detail=f"Could not transcribe file '{target_file_name}'. Error: {str(e)}",
         )
     finally:
         await audio_file.close() 
